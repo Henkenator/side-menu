cc488f34fe717e050cea930aa68543107d15359a
Henkenator <ljunggren.henrik@gmail.com>
2018-10-28 23:00:49 +0100

    code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { DummyComponent } from './dummy/dummy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule
